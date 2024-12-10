1b546f8e295274d97f4a33863fe1a2192db7acc2
daphne1111 <daphane8888@gmail.com>
2024-12-10 15:30:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function draw() {
  
   push();
   fill(225)
-  rect(960,540,300,50);
+  rect(280,540,600,50);
   pop();
   
    if(x>3000){
@@ -22,7 +22,12 @@ function draw() {
   }
   push();
   fill(0);
-  rect(960,540,y,50);
+  rect(380,540,y+10,50);
+  pop();
+  
+  push();
+  fill(0);
+  rect(280,540,100,50);
   pop();
 }
 
@@ -31,8 +36,8 @@ function m(){
   for (let i = -windowWidth; i < windowWidth; i += 50) {
     for (let j = -windowHeight; j < windowHeight; j += 50) {
       noStroke();
-  fill(0,random(60,90));
-  square(i+mouseY*0.5,j+mouseX*0.5,49+sin(frameCount*0.01));
+  fill(random(0,10),random(0,10),random(0,10),random(60,90));
+  square(i+mouseY*0.5,j+mouseX*0.5,49+frameCount*0.0001);
        if(i>3000){
     i=0;
      j=100;
